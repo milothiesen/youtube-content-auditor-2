9b31ade88937d8255d4b31a0f562934d14c8ae3f
Maile Thiessen <mailethiessen@jbauerles-Mac-Pro.local>
2016-10-21 17:15:54 -0400

    made login page root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-	root 'videos#index'
+ #  devise_for :users
+	# root 'videos#index'
+devise_for :users
+
+devise_scope :user do
+  authenticated :user do
+    root 'videos#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
 
 	resources :videos, only: [] do 
 		collection do
